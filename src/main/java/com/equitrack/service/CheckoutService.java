dc033e43f49be775e1dad4e4cde2477da2617f4b
M. <masicar@proton.me>
2025-07-07 13:02:59 -0400

    Finished FormBuilder

--- src/main/java/com/equitrack/service/CheckoutService.java
+++ src/main/java/com/equitrack/service/CheckoutService.java
@@ -28,33 +28,46 @@ public class CheckoutService extends PageBuilder {
 	 */
 	public String buildPage() {
 		Equipment equipment = dao.getEquipment(this.itemId);
-		String html = String.format(
-				"<!DOCTYPE html>\r\n" + "<html lang='en'>\r\n" + "\r\n" + "<head>\r\n"
-						+ "    <meta charset='UTF-8'>\r\n"
-						+ "    <meta name='viewport' content='width=device-width, initial-scale=1.0'>\r\n"
-						+ "    <title>Checkout</title>\r\n" + "    <link rel='stylesheet' href='css/style.css'>\r\n"
-						+ "</head>\r\n" + "\r\n" + "<body>\r\n" + "    <div class='header'>\r\n"
-						+ "        <h1>Checkout %s</h1>\r\n" + "        <div class='header-content'>\r\n"
-						+ "        </div>\r\n" + "    </div>\r\n"
-						+ "    <form class='container-detail edit-form' action='CheckoutForm' method='POST'>\r\n"
-						+ "        <label for='itemName'>Item Name: </label>\r\n"
-						+ "        <input type='text' readonly name='itemName' id='itemName' value='%s'>\r\n"
-						+ "        <label for='itemId'>Item ID: </label>\r\n"
-						+ "        <input type='text' readonly name='itemId' id='itemId' value='%s'>\r\n"
-						+ "        <label for='userFName'>Employee First Name: </label>\r\n"
-						+ "        <input type='text' readonly name='userFName' id='userFName' value='%s'>\r\n"
-						+ "        <label for='userLName'>Employee Last Name: </label>\r\n"
-						+ "        <input type='text' readonly name='userLName' id='userLName' value='%s'>\r\n"
-						+ "        <label for='userId'>Employee ID: </label>\r\n"
-						+ "        <input type='text' readonly name='userId' id='userId' value='%s'>\r\n"
-						+ "        <label for='checkoutDate'>Checkout Date: </label>\r\n"
-						+ "        <input type='date' readonly name='checkoutDate' id='checkoutDate' value='%s'>\r\n"
-						+ "        <label for='returnDate'>Return Date: </label>\r\n"
-						+ "        <input type='date' required name='returnDate' id='returnDate' value=''>\r\n"
-						+ "        <button type='submit'>Submit</button>\r\n" + "    </form>\r\n" + "</body>\r\n"
-						+ "\r\n" + "</html>",
-				equipment.getName(), equipment.getName(), equipment.getId().toString(), user.getFName(),
-				user.getLName(), Integer.toString(user.getId()), Date.valueOf(LocalDate.now()).toString());
+//		String html = String.format(
+//				"<!DOCTYPE html>\r\n" + "<html lang='en'>\r\n" + "\r\n" + "<head>\r\n"
+//						+ "    <meta charset='UTF-8'>\r\n"
+//						+ "    <meta name='viewport' content='width=device-width, initial-scale=1.0'>\r\n"
+//						+ "    <title>Checkout</title>\r\n" + "    <link rel='stylesheet' href='css/style.css'>\r\n"
+//						+ "</head>\r\n" + "\r\n" + "<body>\r\n" + "    <div class='header'>\r\n"
+//						+ "        <h1>Checkout %s</h1>\r\n" + "        <div class='header-content'>\r\n"
+//						+ "        </div>\r\n" + "    </div>\r\n"
+//						+ "    <form class='container-detail edit-form' action='CheckoutForm' method='POST'>\r\n"
+//						+ "        <label for='itemName'>Item Name: </label>\r\n"
+//						+ "        <input type='text' readonly name='itemName' id='itemName' value='%s'>\r\n"
+//						+ "        <label for='itemId'>Item ID: </label>\r\n"
+//						+ "        <input type='text' readonly name='itemId' id='itemId' value='%s'>\r\n"
+//						+ "        <label for='userFName'>Employee First Name: </label>\r\n"
+//						+ "        <input type='text' readonly name='userFName' id='userFName' value='%s'>\r\n"
+//						+ "        <label for='userLName'>Employee Last Name: </label>\r\n"
+//						+ "        <input type='text' readonly name='userLName' id='userLName' value='%s'>\r\n"
+//						+ "        <label for='userId'>Employee ID: </label>\r\n"
+//						+ "        <input type='text' readonly name='userId' id='userId' value='%s'>\r\n"
+//						+ "        <label for='checkoutDate'>Checkout Date: </label>\r\n"
+//						+ "        <input type='date' readonly name='checkoutDate' id='checkoutDate' value='%s'>\r\n"
+//						+ "        <label for='returnDate'>Return Date: </label>\r\n"
+//						+ "        <input type='date' required name='returnDate' id='returnDate' value=''>\r\n"
+//						+ "        <button type='submit'>Submit</button>\r\n" + "    </form>\r\n" + "</body>\r\n"
+//						+ "\r\n" + "</html>",
+//				equipment.getName(), equipment.getName(), equipment.getId().toString(), user.getFName(),
+//				user.getLName(), Integer.toString(user.getId()), Date.valueOf(LocalDate.now()).toString());
+		
+		FormBuilder builder = new FormBuilder();
+		
+		builder.setTitle("Checkout").setAction("CheckoutForm").setMethod("post")
+		.addInput("text", "Item Name:", "itemName", equipment.getName(), true)
+		.addInput("text", "Item ID:", "itemId", equipment.getId(), true)
+		.addInput("text", "Employee First Name:", "userFName", user.getFName(),true)
+		.addInput("text", "Employee Last Name:", "userLName", user.getLName(), true)
+		.addInput("text", "Employee ID:", "userId", Integer.toString(user.getId()), true)
+		.addInput("date", "Checkout Date:", "checkoutDate", Date.valueOf(LocalDate.now()).toString(), true)
+		.addRequiredInput("date", "Return Date:", "returnDate");
+		
+		String html = builder.createForm();
 
 		return html;
 	}
---
+++ src/main/java/com/equitrack/service/FormBuilder.java
@@ -0,0 +1,231 @@
+package com.equitrack.service;
+
+/**
+ * Simplifies and standardizes the creation of html forms.
+ */
+public class FormBuilder {
+	private Form form;
+	private boolean hasAction, defaultMethod;
+
+	/**
+	 * Instantiates a new Form to be created when a FormBuilder
+	 * Object is created.
+	 */
+	public FormBuilder() {
+		form = new Form();
+		hasAction = false;
+		defaultMethod = true;
+	}
+
+	/**
+	 * Adds a new input and its label to the form.
+	 * @param type		The type of input to be generated, eg. "text", "date", etc.
+	 * @param name		The name to be printed in the label.
+	 * @param id		The id and internal name of the input.
+	 * @return
+	 */
+	public FormBuilder addInput(String type, String name, String id) {
+		form.addInput(String.format(
+				"<label for='%s'>%s</label>"
+						+ "<input type='%s' name='%s' id='%s' value=''>",
+						id, name,
+						type, id, id));
+
+		return this;
+	}
+
+	/**
+	 * Adds a new input and its label to the form.
+	 * @param type		The type of input to be generated, eg. "text", "date", etc.
+	 * @param name		The name to be printed in the label.
+	 * @param id		The id and internal name of the input.
+	 * @param value		The initial value to be placed inside the input.
+	 * @return
+	 */
+	public FormBuilder addInput(String type, String name, String id, String value) {
+		form.addInput(String.format(
+				"<label for='%s'>%s</label>"
+						+ "<input type='%s' name='%s' id='%s' value='%s'>",
+						id, name,
+						type, id, id, value));
+
+		return this;
+	}
+
+	/**
+	 * Adds a new input and its label to the form.
+	 * @param type			The type of input to be generated, eg. "text", "date", etc.
+	 * @param name			The name to be printed in the label.
+	 * @param id			The id and internal name of the input.
+	 * @param value			The initial value to be placed inside the input.
+	 * @param isReadOnly	Whether or not the field should be editable.
+	 * @return
+	 */
+	public FormBuilder addInput(String type, String name, String id, String value, boolean isReadOnly) {
+		String readOnly = isReadOnly ? "readonly" : "";
+		form.addInput(String.format(
+				"<label for='%s'>%s</label>"
+						+ "<input type='%s' %s name='%s' id='%s' value='%s'>",
+						id, name,
+						type, readOnly, id, id, value));
+
+		return this;
+	}
+
+	/**
+	 * Adds a new required input and its label to the form.
+	 * @param type		The type of input to be generated, eg. "text", "date", etc.
+	 * @param name		The name to be printed in the label.
+	 * @param id		The id and internal name of the input.
+	 * @return
+	 */
+	public FormBuilder addRequiredInput(String type, String name, String id) {
+		form.addInput(String.format(
+				"<label for='%s'>%s</label>"
+						+ "<input type='%s' required name='%s' id='%s' value=''>",
+						id, name,
+						type, id, id));
+		
+		return this;
+	}
+	
+	/**
+	 * Adds a new required input and its label to the form.
+	 * @param type		The type of input to be generated, eg. "text", "date", etc.
+	 * @param name		The name to be printed in the label.
+	 * @param id		The id and internal name of the input.
+	 * @param value		The initial value to be placed inside the input.
+	 * @return
+	 */
+	public FormBuilder addRequiredInput(String type, String name, String id, String value) {
+		form.addInput(String.format(
+				"<label for='%s'>%s</label>"
+						+ "<input type='%s' required name='%s' id='%s' value='%s'>",
+						id, name,
+						type, id, id, value));
+
+		return this;
+	}
+
+	/**
+	 * Adds a new hidden input to the form.
+	 * @param id		The internal name and id of the input.
+	 * @param value		The value to be stored in the hidden input.
+	 * @return
+	 */
+	public FormBuilder addHiddenInput(String id, String value) {
+		form.addInput(String.format(
+				"<input type='hidden' name='%s' id='%s' value='%s'>",
+				id, id, value));
+
+		return this;
+	}
+
+	/**
+	 * Sets the action of the form, usually the current Servlet's
+	 * declared URL pattern. Defaults to an empty String.
+	 * @param formAction
+	 * @return
+	 */
+	public FormBuilder setAction(String formAction) {
+		form.setFormAction(String.format("action='%s'", formAction));
+		this.hasAction = true;
+
+		return this;
+	}
+
+	/**
+	 * Sets the method of the form. Defaults to post.
+	 * @param formMethod
+	 * @return
+	 */
+	public FormBuilder setMethod(String formMethod) {
+		form.setFormMethod(String.format("method='%s'", formMethod));
+		this.defaultMethod = false;
+
+		return this;
+	}
+
+	/**
+	 * Sets the title of the page to be displayed in the browser tab
+	 * and above the form.
+	 * @param formTitle
+	 * @return
+	 */
+	public FormBuilder setTitle(String formTitle) {
+		form.setFormTitle(formTitle);
+
+		return this;
+	}
+
+	/**
+	 * Removes the submit button from the form.
+	 * @return
+	 */
+	public FormBuilder removeSubmit() {
+		form.setHasSubmit(false);
+
+		return this;
+	}
+
+	/**
+	 * Adds a reset button to the form.
+	 * @return
+	 */
+	public FormBuilder addReset() {
+		form.setHasReset(true);
+
+		return this;
+	}
+
+	/**
+	 * Adds an error message to the form under the submit/reset buttons.
+	 * @param errorMessage
+	 * @return
+	 */
+	public FormBuilder addError(String errorMessage) {
+		form.setErrorMessage(String.format("<p>%s<p>", errorMessage));
+
+		return this;
+	}
+
+	/**
+	 * Generates the html using the parameters set by previous method calls.
+	 * @return
+	 */
+	public String createForm() {
+		StringBuilder sb = new StringBuilder();
+		String errorMessage = form.getErrorMessage();
+		
+		String formTitle = form.getFormTitle();
+		String formAction = this.hasAction ? form.getFormAction() : "";
+		String formMethod = this.defaultMethod ? "method='post'" : form.getFormMethod();
+
+		sb.append("<!DOCTYPE html><html lang='en'><head>")
+		.append("<meta charset='UTF-8>")
+		.append("<meta name='viewport' content='width=device-width, initial-scale='1.0'>")
+		.append(errorMessage != null ? "<style>p {color: red; text-align: center}</style>" : "")
+		.append(String.format("<title>%s</title>", formTitle))
+		.append("<link rel='stylesheet' href='css/style.css'>")
+		.append("</head>")
+		.append("<body>")
+		.append("<div class='header'>")
+		.append(String.format("<h1>%s<h1>", formTitle))
+		.append("<div class='header-content'>")
+		.append("</div></div>")
+		.append("<form class='container-detail edit-form'" + formAction + formMethod + ">");
+
+		for (String input : form.getInputs()) {
+			sb.append(input);
+		}
+
+		sb.append("<div class='form-buttons'>")
+		.append(form.getHasSubmit() ? "<button type='submit'>Submit</button>" : "")
+		.append(form.getHasReset() ? "<button type='reset'>Reset</button>" : "")
+		.append("</div></form>")
+		.append(errorMessage)
+		.append("</body></html>");
+
+		return sb.toString();
+	}
+}
